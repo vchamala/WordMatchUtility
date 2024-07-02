9fa8538cc06dc36205a05a1a5fe0aa15c40998c6
vchamala <49884971+vchamala@users.noreply.github.com>
2024-07-02 12:20:10 -0700

    Update WordMatchUtility.java

--- src/main/java/org/example/WordMatchUtility.java
+++ src/main/java/org/example/WordMatchUtility.java
@@ -19,8 +19,14 @@ public class WordMatchUtility {
 
         /* 2. Go through each line of the file and find the matches and pretty prints the matched word and the occurrence count. */
         String inputFile = "../../../resources/inputFile.txt"; // can be replaced by your local file for tests.
-        System.out.println("Predefined word     Match Count");
+        System.out.println("Predefined word     Exact Match Count");
         for (Map.Entry<String, Integer> entry: util.findMatches(inputFile, predefinedWords).entrySet()) {
+            // Inclining that the largest number will be of 20 characters.
+            System.out.printf("%-20s %d%n", entry.getKey(), entry.getValue());
+        }
+
+        System.out.println("Predefined word     Exact Match and Substring Match Count");
+        for (Map.Entry<String, Integer> entry: util.findMatchesIncludingSubStrings(inputFile, predefinedWords).entrySet()) {
             // Inclining that the largest number will be of 20 characters.
             System.out.printf("%-20s %d%n", entry.getKey(), entry.getValue());
         }
@@ -58,6 +64,38 @@ public class WordMatchUtility {
         } catch (IOException ex) {
             System.err.printf("IOException when finding matches: %s%n", ex.getMessage());
             throw ex;
+        }
+    }
+
+    /*
+    The method finds the exact matches as well as the substring matches of the input file's words with predetermined words.
+
+    Due to the provided input file could be 20mb (larger file),
+    1. we are reading the predetermined words as step1.
+    2. we look through each of the word in every line of the file to check if it is a match or a substring match in predetermined words.
+     */
+    private Map<String, Integer> findMatchesIncludingSubStrings(String inputFileName, Set<String> predefinedWords) throws IOException {
+        Map<String, Integer> wordOccurencesMap = new HashMap<>();
+        try (BufferedReader br
+                     = new BufferedReader(new FileReader(inputFileName))) {
+            String st;
+            while ((st = br.readLine()) != null) {
+                String[] lineWords = st.split(" ");
+                for (String lineWord: lineWords) {
+                    for (String predefinedWord: predefinedWords) {
+                        if (lineWord.contains(predefinedWord)) {
+                            wordOccurencesMap.put(predefinedWord, wordOccurencesMap.getOrDefault(predefinedWord, 0) + 1);
+                        }
+                    }
+                }
+            }
+            return wordOccurencesMap;
+        } catch (IOException ex) {
+            System.err.printf("IOException when processing predefined words: %s%n", ex.getMessage());
+            throw ex;
+        } catch (Exception e) {
+            System.err.printf("Exception when processing predefined words: %s%n", e.getMessage());
+            throw e;
         }
     }
 
