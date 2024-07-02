a3f8d956260f43b4134e3250e10c5bc5b29adafe
vchamala <49884971+vchamala@users.noreply.github.com>
2024-07-01 21:43:09 -0700

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,28 @@
+Word Match Utility:
+
+Description:
+Write a program that reads a file and finds matches against a predefined set of words. There can be up to 10K entries in the list of predefined words.
+Requirement details:
+1. Input file is a plain text (ascii) file, every record separated by a new line.
+2. For this exercise, assume English words only
+3. The file size can be up to 20 MB
+4. The predefined words are defined in a text file, every word separated by a newline. Use a sample file of your choice for the set of predefined keywords for the exercise.
+
+Code:
+src/main/java/org/example/WordMatchUtility.java
+
+Command Line Run:
+java {ProjectLocationPath}/testProject/src/main/java/org/example/WordMatchUtility.java
+
+Algorithm:
+As the input file could be up to 20mb, it is optimal to load all the predefined words into a Set as the first step, and
+then while reading the input file, we check for the matching words.
+Steps:
+1. Load all the predefined words into a Set. Use BufferedReader to read the file, as this reads line-by-line of a file.
+(every word separated by a newline in the file.)
+2. Read the input file, and for each line, verify if there is a matched word. Use BufferedReader to read the file as we
+read the file line-by-line.
+
+Tests:
+I have not included unit tests in the initial revision, but tests would be advised and can add the tests if we would
+want them in scope.
---
+++ WordMatchUtility.java
@@ -0,0 +1,71 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class WordMatchUtility {
+
+    public static void main(String[] args) throws IOException {
+        WordMatchUtility util = new WordMatchUtility();
+
+        /* 1. Load all the predefined words. */
+        String predefinedWordsFile = "../../../resources/predefinedWords.txt"; // can be replaced by your local file for tests.
+        Set<String> predefinedWords = util.readPredefinedWords(predefinedWordsFile);
+
+        /* 2. Go through each line of the file and find the matches and prints the word as we are looking to find only
+        the matches and pretty print the matched word and the occurrence count. */
+        String inputFile = "../../../resources/inputFile.txt"; // can be replaced by your local file for tests.
+        System.out.println("Predefined word     Match Count");
+        for (Map.Entry<String, Integer> entry: util.findMatches(inputFile, predefinedWords).entrySet()) {
+            // Inclining that the largest number will be of 20 characters.
+            System.out.printf("%-20s %d%n", entry.getKey(), entry.getValue());
+        }
+    }
+
+    private Set<String> readPredefinedWords(String predefinedWordsFileName) throws IOException {
+        Set<String> predefinedWords = new HashSet<>();
+        try (BufferedReader br
+                = new BufferedReader(new FileReader(predefinedWordsFileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                predefinedWords.add(line.trim()); // trim any boundary spaces present in the lines
+            }
+        } catch (IOException ex) {
+            System.err.printf("IOException when processing predefined words: %s%n", ex.getMessage());
+            throw ex;
+        } catch (Exception e) {
+            System.err.printf("Exception when processing predefined words: %s%n", e.getMessage());
+            throw e;
+        }
+        return predefinedWords;
+    }
+
+    private Map<String, Integer> findMatches(String inputFileName, Set<String> predefinedWords) throws IOException {
+        Map<String, Integer> wordOccurencesMap = new HashMap<>();
+        try (BufferedReader br
+                = new BufferedReader(new FileReader(inputFileName))) {
+            String st;
+            while ((st = br.readLine()) != null) {
+                String[] lineWords = st.split(" ");
+                for (String lineWord: lineWords) {
+                    if (predefinedWords.contains(lineWord)) {
+                        wordOccurencesMap.put(lineWord, wordOccurencesMap.getOrDefault(lineWord, 0) + 1);
+                    }
+                }
+            }
+            return wordOccurencesMap;
+        } catch (IOException ex) {
+            System.err.printf("IOException when processing predefined words: %s%n", ex.getMessage());
+            throw ex;
+        } catch (Exception e) {
+            System.err.printf("Exception when processing predefined words: %s%n", e.getMessage());
+            throw e;
+        }
+    }
+
+}
